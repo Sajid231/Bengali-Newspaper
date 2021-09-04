52a946d8f2c58595bf16fcd41898ea13995ce187
Zubaer Ahmed <zubaer.ahmed17@northsouth.edu>
2021-09-04 15:01:12 +0600

    added asgi.py

---
+++ Project code/Source/BanglaNewsPaper/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from django.views.generic import TemplateView
+from news import urls as news_urls
+
+urlpatterns = [
+
+    # Home Page
+    path('', TemplateView.as_view(template_name="index.html"), name="Index"),
+    # News Page
+    path("", include(news_urls)),
+]
